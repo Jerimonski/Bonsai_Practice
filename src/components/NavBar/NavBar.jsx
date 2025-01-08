7648684f60190993ef247fb8b2cad738ae59a0d9
jeri <jerimonski@gmail.com>
2025-01-08 19:40:17 -0300

    ✨ Dropdown Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ function NavBar() {
   }
 
   return (
-    <div className="fixed z-50 bg-white w-full flex items-center justify-center py-4 px-4 gap-32 place-content-evenly cursor-pointer">
+    <div className="fixed z-50 bg-white w-full flex items-center justify-center py-2 px-4 gap-32 place-content-evenly cursor-pointer">
       <div className="h-22 w-24">
         <Link to={"/Home"}>
           <img className="" src="BonsaiLogo.png" alt="BonsaiLogo" />
@@ -19,20 +19,49 @@ function NavBar() {
       <nav>
         <ul className="flex w-full gap-10">
           <li
-            className="flex items-center gap-2"
+            className="flex items-center gap-2 py-4"
             onMouseEnter={handleOnMouse}
-            onMouseLeave={handleOnMouse}
           >
+            Product
+            <FontAwesomeIcon icon={faChevronDown} />
+            {onMouse && (
+              <div className="absolute top-16 bg-white w-[350px] shadow-2xl">
+                <div className="hover:bg-gray-100 px-10 py-12">
+                  <h2 className="text-xl">Bonsai Workflow</h2>
+                  <p className="text-gray-600">
+                    Look professional, win more clients and manage your business
+                    from one place
+                  </p>
+                  <div className="relative top-12 border-t border-gray-200"></div>
+                </div>
+                <div className="hover:bg-gray-200 px-10 py-12">
+                  <h2 className="text-xl">Bonsai Tax</h2>
+                  <p className="text-gray-600">
+                    Track expenses, maximize tax write-offs, and estimate taxes
+                    without the headache
+                  </p>
+                  <div className="relative top-12 border-t border-gray-200"></div>
+                </div>
+                <div className="hover:bg-gray-200 px-10 py-12">
+                  <h2 className="text-xl">Bonsai Cash</h2>
+                  <p className="text-gray-600">
+                    Bonsai’s all-in-one financial hub: No fees and lightning
+                    fast payouts
+                  </p>
+                </div>
+              </div>
+            )}
+          </li>
+          <li className="flex items-center gap-2 py-4">
             Templates
             <FontAwesomeIcon icon={faChevronDown} />
-            {onMouse && <div>dropdownMenu</div>}
           </li>
-          <li className="flex items-center gap-2">
-            Templates
-            <FontAwesomeIcon icon={faChevronDown} />
-          </li>
-          <Link to={"/Pricing"}>Pricing</Link>
-          <Link to={"/Reviews"}>Reviews</Link>
+          <Link to={"/Pricing"} className="py-4">
+            Pricing
+          </Link>
+          <Link to={"/Reviews"} className="py-4">
+            Reviews
+          </Link>
         </ul>
       </nav>
       <div className="flex gap-4">
